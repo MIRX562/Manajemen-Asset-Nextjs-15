64f4667abb741cdbab0d5642bea3dbef776804cc
mirx562 <mirx562@gmail.com>
2025-02-11 00:03:01 +0700

    now

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
       script: "npm",
       args: "run start",
       watch: true,
-      instances: "max", // Use all CPU cores
-      exec_mode: "cluster", // Cluster mode for better performance
+      instances: "max",
+      exec_mode: "cluster",
       autorestart: true,
     },
   ],
