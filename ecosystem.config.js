d8b6e533b8faf5edca53e52ac55be1586b8bba15
mirx562 <mirx562@gmail.com>
2025-03-09 00:26:12 +0700

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,14 @@
-export default {
+module.exports = {
   apps: [
     {
-      name: "am-kp",
-      script: "server.js",
+      name: "myapp",
+      script: "./server.js",
       watch: true,
-      instances: 4,
-      exec_mode: "fork",
-      autorestart: true,
+      env: {
+        NODE_ENV: "production",
+        DATABASE_URL:
+          "postgresql://neondb_owner:UZlt2fH0QEud@ep-raspy-haze-a59x6v9m.us-east-2.aws.neon.tech/neondb?sslmode=require",
+      },
     },
   ],
 };
